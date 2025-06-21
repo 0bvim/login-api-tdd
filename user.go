c220127a6ba84066bc2b08cf485f14fed4043f8b
vinicius <niviciusdev@gmail.com>
2025-06-21 00:24:42 -0300

    Add password hashing and verification to User struct

--- user.go
+++ user.go
@@ -1,6 +1,10 @@
 package main
 
-import "regexp"
+import (
+	"regexp"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	Email    string `json:"email"`
@@ -11,4 +15,19 @@ func (u *User) ValidateEmail() bool {
 	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
 
 	return emailRegex.MatchString(u.Email)
+}
+
+func (u *User) HashPassword() error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+
+	return nil
+}
+
+func (u *User) checkPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
 }
--- user_test.go
+++ user_test.go
@@ -19,3 +19,25 @@ func TestUser_ValidateEmail_Invalid(t *testing.T) {
 	valid := user.ValidateEmail()
 	assert.False(t, valid, "Invalid email should return false")
 }
+
+func TestUser_HashPassword(t *testing.T) {
+	user := User{Email: "test@example.com", Password: "plainpassword"}
+	originalPassword := user.Password
+
+	err := user.HashPassword()
+
+	assert.NoError(t, err, "Hashing password should not return error")
+	assert.NotEqual(t, originalPassword, user.Password, "Password should be hashed")
+	assert.True(t, len(user.Password) > 0, "Hashed password should not be empty")
+}
+
+func TestUser_CheckPassword(t *testing.T) {
+	user := User{Email: "test@example.com", Password: "plainpassword"}
+	user.HashPassword()
+
+	valid := user.checkPassword("plainpassword")
+	assert.True(t, valid, "Correct password should return true")
+
+	invalid := user.checkPassword("notplainpassword")
+	assert.False(t, invalid, "Invalid password should return false")
+}
